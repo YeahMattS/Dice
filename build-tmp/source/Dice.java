6202d5f7682d4622a4fb1e683966540e7c52304b
yeahmatts <mattyeh987@gmail.com>
2015-09-10 07:57:20 -0700

    added dice + stat distributions

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,4 +1,20 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
+public void setup()
 {
 	size(610, 610);
 	background(0);
@@ -13,7 +29,7 @@ float num4;
 float num5;
 float num6;
 
-void draw()
+public void draw()
 {
 	sum = 0;
 	num1 = 0;
@@ -64,7 +80,7 @@ void draw()
 }
 
 
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -76,11 +92,11 @@ class Die //models one single dice cube
 		this.x = x;
 		this.y = y;
 	}
-	void roll()
+	public void roll()
 	{
 		dot = (int)(Math.random()*6)+1;
 	}
-	void show()
+	public void show()
 	{
 		fill(255);
 		rect(x, y, 40, 40);
@@ -140,3 +156,12 @@ class Die //models one single dice cube
 	}
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
